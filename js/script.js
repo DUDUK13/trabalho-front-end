abf0875471743993cc8b0b9f6f89c4d1ef9b3d0c
DUDUK13 <eduardo.eduardoborgesxbox@gmail.com>
2025-03-27 15:21:54 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function irParaProjeto() {
+    document.getElementById("projeto").scrollIntoView({behavior: "smooth"});
+}
+function irParaintegrantes() {
+    document.getElementById("integrantes").scrollIntoView({behavior: "smooth"});
+}
